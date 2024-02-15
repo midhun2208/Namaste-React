6f794880477c4185b8ec828f6fa1aad3eda8dfc5
midhun2208 <midhun.sm343@gmail.com>
2024-02-15 18:04:25 +0530

    episode-02

---
+++ App.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+//React.crerateElement => Objevtct => HTMLElement(render)
+
+const elem = <span>Hey</span>
+
+const Title = () => {
+    return <h1>Hi  iam  title heading</h1>
+}
+
+const title = (
+    <h1>
+        {elem}
+        element
+    </h1>
+)
+
+const heading = React.createElement("h1", { id: "heading" }, "HI iam Heading")
+
+
+// JSX => Babel is transpiled it to  React.createElement => React Element => JS Object => HTML Element(render)
+
+//Jsx (transpiled before it reaches the JS ) -PARCEL  -Babel
+
+const jsxHeading = (<h1 className='head'>This is from Jsx</h1>)
+
+//React Functional Component
+
+
+//Component  Composition - Where component in another component
+//And also the title in the component is a React element using JSX {title}
+const HeaderComponent = () => {
+
+    return <>
+        <Title />
+        {title}
+        <h1 className='heading'> Namaste React Functional Component</h1>
+    </>
+}
+
+const HeaderComponent2 = () => (
+    <div>
+        <Title />
+        <h2>heyy</h2>
+    </div>
+)
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeaderComponent />);
---
+++ ReadMe.md
@@ -0,0 +1,31 @@
+# Namaste React
+
+# Parcel
+
+- Dev build
+- Local Server
+- HMR - Hot Module Replacement
+
+- File Watching Algorithem
+- Caching - Faster Builds
+- Minification
+- Bundling
+- Compressing
+- Differenvcial bundling - runs in different old browser
+- error handling
+- Https
+- Tree shaking - remove unsed codes
+- Different dev and production bundles
+
+# Components
+
+1. Class Based components - OLD WAY
+2. Functional Based Components - NEW Way and also the functional component is a normal js function that return some jsx
+
+# Component Composition
+
+- Where component in another component
+# JSX
+Java Script Xml 
+it is like a HTML code 
+where there is "{}" we can write ant JS code in it
